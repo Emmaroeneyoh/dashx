b31a08cf9128a5e23031f6ef953893d56d84ac2f
emmaro <emmaro@gmail.com>
2024-01-07 07:13:09 +0100

    first commit

--- user/controller/wallet.js
+++ user/controller/wallet.js
@@ -77,7 +77,7 @@ const userfundwalletController = async (req, res, next) => {
   const { data , customer } = req.body;
   try {
     const usertype = data.metadata.usertype
-    console.log(customer)
+    console.log('this is customer ' , customer)
     if (usertype == 'user') {
       const email = data.customer.email
       const userEmail = email.toLowerCase();
