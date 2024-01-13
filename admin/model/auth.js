c2471ea159a0327a18d076ac36b3b1256118df4f
emmaro <emmaro@gmail.com>
2024-01-13 21:13:30 +0100

    first commit

--- admin/model/auth.js
+++ admin/model/auth.js
@@ -1,17 +1,20 @@
 const { adminModel } = require("../core/db/admin");
+const { adminroleModel } = require("../core/db/role");
 const { create_admin_token } = require("../core/utils");
 
 
 const adminLoginModel = async (data,res) => {
   try {
     const { userEmail, } = data
-     const userDetails = await adminModel.findOne({ email:userEmail});
+    const userDetails = await adminModel.findOne({ email: userEmail });
+    const roles = userDetails.roles
+    const adminrole = await  adminroleModel.find({ _id: { $in: roles } })
      const token = create_admin_token(userDetails._id)
      const userData = {
          id: userDetails._id,
          name: userDetails.name,
          email: userDetails.email,
-         token,
+         token, adminrole 
       }
    
      return userData
